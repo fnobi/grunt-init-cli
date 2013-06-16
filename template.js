7b6c36752b3e820af6ae79d3a43c544afec699d5
Fujisawa Shin <fujisawa-shin@in.kayac.com>
2013-06-16 23:40:35 +0900

    first commit.

---
+++ template.js
@@ -0,0 +1,30 @@
+exports.description = 'web page template (compass + embed require + mocha + koko)';
+
+// Template-specific notes to be displayed before question prompts.
+exports.notes = '';
+
+// Template-specific notes to be displayed after question prompts.
+exports.after = '';
+
+// Any existing file or directory matching this wildcard will cause a warning.
+exports.warnOn = '*';
+
+// The actual init template.
+exports.template = function (grunt, init, done) {
+    init.process( {}, [
+        init.prompt('name'),
+        init.prompt('description')
+    ], function(err, props) {
+        // Files to copy (and process).
+        var files = init.filesToCopy(props);
+
+        // Actually copy (and process) files.
+        init.copyAndProcess(files, props, {});
+
+        // executable
+        require('fs').chmodSync('bin/' + props.name, '0755');
+
+        // All done!
+        done();
+    });
+};
